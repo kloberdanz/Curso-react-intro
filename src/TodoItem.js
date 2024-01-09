9c2d66e68c7050ff364b6fa782184e74fa4f1141
Lucas Kloberdanz <klobermdq@gmail.com>
2024-01-09 03:49:44 -0300

    more distribution folders

---
+++ src/TodoItem.js
@@ -0,0 +1,12 @@
+
+function TodoItem(){
+    return(
+      <li>
+        <span>V</span>
+        <p>A llorar a la lloreria</p>
+        <span>X</span>
+      </li>
+    );
+  }
+
+  export { TodoItem };
