636001f244ddf32ca18e966917035f5b144b21c9
Lucas Kloberdanz <klobermdq@gmail.com>
2024-06-15 08:10:27 -0300

    add check css styles to list item

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -4,8 +4,8 @@ import "./TodoItem.css";
 function TodoItem(props){
     return(
       <li>
-        <span className="check check--active"></span>
-        <p className="complete">{props.text}</p>
+        <span className={`check ${props.completed && "check--active"} `}></span>
+        <p className={` ${props.completed && "complete "} `}>{props.text}</p>
         <span className="cancel"></span>
       </li>
     );
