58b61febad7c5f6d4183fabc6c0d701d327d0365
Lucas Kloberdanz <klobermdq@gmail.com>
2024-07-01 00:12:51 -0300

    coso

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,13 +1,16 @@
+import React from "react";
 import "./TodoSearch.css";
 
 function TodoSearch(){
+  const [searchValue, setSearchValue] = React.useState("");
+
+  console.log("Los USUARIOS buscan todos de " + searchValue );
+
     return(
       <input 
+      value={searchValue}
       onChange={ (event) => {
-        console.log("escribiste ");
-        console.log(event);
-        console.log(event.target.value );
-        console.log(event.target);
+        setSearchValue( event.target.value );
 
       }}
       placeholder="Cortar cebolla" />
